57585f3ff5a037ef5d66bf447137fe22ebabf682
mateohenao <matteohenao36@gmail.com>
2022-05-21 00:08:32 -0500

    Add to cart part2

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -3,12 +3,14 @@ import { MinicartComponent } from './components/minicart/minicart.component';
 import { CommonModule } from "@angular/common";
 import { BrowserModule } from "@angular/platform-browser";
 import { AddToCartComponent } from './components/modal/add-to-cart/add-to-cart.component';
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
-    imports: [
-        CommonModule,
-        BrowserModule
-    ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    ReactiveFormsModule
+  ],
     exports: [
         MinicartComponent
     ],
--- src/app/cart/components/modal/add-to-cart/add-to-cart.component.ts
+++ src/app/cart/components/modal/add-to-cart/add-to-cart.component.ts
@@ -4,6 +4,7 @@ import { ProductService } from "../../../../product/services/product.service";
 import { Product } from "../../../../shared/model/product";
 import { Subscription } from "rxjs";
 import { AlertService } from "../../../../shared/services/alert/alert.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-add-to-cart',
@@ -16,9 +17,28 @@ export class AddToCartComponent implements OnInit, OnDestroy {
   product: Product | undefined;
   productSubscription : Subscription | undefined;
 
+  addToCartForm: FormGroup;
+  maxQty: number = 0;
+  submitted = false;
+
   constructor(private activeModal: NgbActiveModal,
               private productService: ProductService,
-              private alertService: AlertService) { }
+              private alertService: AlertService,
+              private formBuilder: FormBuilder) {
+    this.addToCartForm = this.formBuilder.group({
+      qty: [0, [Validators.required, Validators.min(1), Validators.max(10)]]
+    });
+  }
+
+  get f(): any { return this.addToCartForm?.controls; }
+
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.addToCartForm.invalid) {
+      return;
+    }
+    alert('ITs going to add something here');
+  }
 
   closeModal(reason: any): any {
     this.activeModal.close(reason);
@@ -36,6 +56,14 @@ export class AddToCartComponent implements OnInit, OnDestroy {
       this.productSubscription = this.productService.getProductById(this.productId).subscribe({
         next: value => {
           this.product = value;
+          if(this.product && this.product.stock && this.product.stock.remainingQuantity){
+            this.maxQty = this.product.stock.remainingQuantity;
+
+            this.addToCartForm = this.formBuilder.group({
+              qty: [0, [Validators.required, Validators.min(1), Validators.max(this.maxQty)]]
+            });
+
+          }
         },
         error: err => {
           this.alertService.showDanger(err.error.error);
